4a0ba832f22367a62d2c171e4bdc6435018ab191
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2022-05-03 13:29:02 +0800

    增加异步预约（待测试），修改预约逻辑

--- README.md
+++ README.md
@@ -3,28 +3,51 @@
 
 支持通过统一身份认证登录进行中区体育馆场地预约（由于游泳预约平台已经迁移，目前该脚本只支持预约羽毛球场）
 
-如果你强行令变量gymnasium_id为4，将可以在小程序中获取到一张游泳馆入场券，但我估计这个入场券是无效的
-
 本项目仅供学习使用
 
 # 环境
 
-python=3.6
+python=3.6+
 
 见requirements.txt
 
 # 使用方法
+
+首先在config.yml中填写需要的字段（注意冒号与值之间需要有空格），例如：
+
+```yaml
+username: SA66668888
+password: 12345678
+time_ids: [3, 4, 5, 6]
+people_number: 2
+```
+其中，time_ids为一个list，包含你想预约的全部时间段的id，对应关系如下：
+
+|时间段|id|
+|---|---|
+|08:00-09:30|3|
+|09:30-11:00|4|
+|11:00-12:30|5|
+|12:30-14:00|6|
+|14:00-15:30|7|
+|15:30-17:00|8|
+|17:00-18:30|9|
+|18:30-20:00|10|
+|20:00-21:30|11|
+
+例如，若你想预约14:00-21:30之间任意场次，就填: [7, 8, 9, 10, 11]
+
+然后直接按下面方法调用脚本：
+
 ```python
 from ustc_gym_appointment import USTCGymAppointment
 
 bot = USTCGymAppointment()
-# 登录
-bot.login('SAxxxxxxxx', 'password')
-# 可通过简单交互程序进行预约
-bot.interact()
-# 也可通过bot.submit方法自行调用预约,需要自己构造params参数
-bot.submit(params)
+bot.appointment()
+```
+
+取消预约方法：
+```python
 # 通过bot.cancel方法取消预约,参数reserve_id在预约成功后的返回结果中获取
 bot.cancel(reserve_id)
-# 使用bot.find_available方法获取四天内的可用场地
-available = bot.find_available(gymnasium_id=1)
+```
--- ustc_gym_appointment.py
+++ ustc_gym_appointment.py
@@ -2,6 +2,9 @@ import datetime
 import json
 import urllib.parse
 import tqdm
+import asyncio
+import aiocqhttp
+import yaml
 
 from ustc_passport_login import USTCPassportLogin
 
@@ -48,80 +51,7 @@ class USTCGymAppointment(object):
         r = self.sess.post(self.token_url, data=json.dumps(data), headers=headers)
         return r.json().get('data').get('token')
 
-    def _get_available(self, gymnasium_id, date_str, time_quantum_id):
-        """
-        寻找某一天某一时间段的可用场地
-        """
-        data = {
-            "gymnasiumId": gymnasium_id,
-            "dateStr": date_str,
-            "timeQuantumId": time_quantum_id
-        }
-        headers = {"content-type": "application/json", "token": self.token}
-        result = self.sess.post(self.info_url, data=json.dumps(data), headers=headers).json()
-        code = result.get('code')
-        if code != 200:
-            return False, result.get('msg')
-        available = [data.get('id') for data in result.get('data') if data.get('useType') == 0]
-        return True, available
-
-    def _choose_sport(self):
-        """
-        选择羽毛球或游泳
-        """
-        print('目前游泳预约已切换平台，强制选择羽毛球')
-        return 1
-        while 1:
-            choise = input("选择想要预约的运动类型:\n0: 退出\n1: 羽毛球\n2: 游泳") or '1'
-            if choise.isdigit() and int(choise) in {0, 1, 2}:
-                break
-        if choise == '0':
-            return
-        return {'1': 1, '2': 4}.get(choise)
-
-    def _choose_date(self, available_dict):
-        dates = list({key[0] for key in available_dict.keys()})
-        dates.sort()
-        msg = f"请选择预约日期:\n0: 退出\n"
-        for i, date in enumerate(dates, 1):
-            msg += f'{i}: {date}\n'
-        while 1:
-            choise = input(msg) or '1'
-            if choise.isdigit() and int(choise) in range(i + 1):
-                break
-        if choise == '0':
-            return
-        selected_date = dates[int(choise) - 1]
-        return selected_date
-
-    def _choose_time(self, available_dict, date):
-        times = [key[1] for key in available_dict.keys() if key[0] == date]
-        times.sort()
-        msg = f"请选择预约时间段:\n0: 退出\n"
-        for i, t in enumerate(times, 1):
-            msg += f'{i}: {self.id2time[t]}\n'
-        while 1:
-            choise = input(msg) or '1'
-            if choise.isdigit() and int(choise) in range(len(times) + 1):
-                break
-        if choise == '0':
-            return
-        return times[int(choise) - 1]
-
-    def _choose_available_place(self, available):
-        while 1:
-            choise = input(f"可用场地: {available}\n请选择(0: 退出):\n") or str(available[0])
-            if choise.isdigit() and (choise == '0' or int(choise) in available):
-                return int(choise)
-
-    def _choose_people_number(self, gymnasium_id):
-        default = {1: '2', 4: '1'}.get(gymnasium_id)
-        while 1:
-            people_number = input(f"请填写入场人数(1~10):\n羽毛球默认2人、游泳默认1人\n0: 退出\n") or default
-            if people_number.isdigit() and 0 <= int(people_number) <= 10:
-                return int(people_number)
-
-    def login(self, username, password):
+    def _login(self, username, password):
         """
         登录,需要提供用户名、密码，顺便返回后续表单需要提供的token
         """
@@ -132,29 +62,14 @@ class USTCGymAppointment(object):
             self.token = self._get_token(ticket)
         return is_success
 
-    def find_available(self, gymnasium_id):
-        """
-        寻找两天内的可用场地
-        返回一个字典,其键为(date, time_quantum_id)二元组,值为可用场地的list
-        """
-        today = datetime.datetime.now()
-        one_day = datetime.timedelta(days=1)
-        dates = [(today + one_day * i).strftime("%Y-%m-%d") for i in range(2)]
-        available_dict = {}
-        for date in tqdm.tqdm(dates):
-            for time_quantum_id in self.id2time.keys():
-                is_success, available = self._get_available(gymnasium_id, date, time_quantum_id)
-                if is_success and available:
-                    available_dict[(date, time_quantum_id)] = available
-        return available_dict
-
+    @aiocqhttp.ensure_async
     def submit(self, gymnasium_id, sport_place_id, time_quantum_id,
-               user, people_number, appointment_day, phone):
+               user, people_number, appointment_day, phone, success_list):
         """
         提交预约请求,需要提供预约表单的参数
         成功时返回预约信息，失败时返回预约失败的msg
         """
-        data = {
+        post_data = {
             "gymnasiumId": gymnasium_id,
             "sportPlaceId": sport_place_id,
             "timeQuantum": self.id2time[time_quantum_id],
@@ -168,11 +83,15 @@ class USTCGymAppointment(object):
             "content-type": "application/json",
             "token": self.token
         }
-        result = self.sess.post(self.submit_url, data=json.dumps(data), headers=headers).json()
+        result = self.sess.post(self.submit_url, data=json.dumps(post_data), headers=headers).json()
         code = result.get('code')
         if code != 200:
+            print(f"{post_data['timeQuantum']}, 场地{sport_place_id}", result.get('msg'), "\n")
             return False, result.get('msg')
-        return True, result.get('data')
+        data = result.get('data')
+        data['time'] = post_data['timeQuantum']
+        success_list.append(data)
+        return True, data
 
     def cancel(self, reserve_id):
         """
@@ -186,45 +105,42 @@ class USTCGymAppointment(object):
         r = self.sess.post(url, headers=headers).json()
         return r.get('code') == 200, r.get('msg')
 
-    def interact(self):
-        """
-        简单的人机交互程序
-        """
-        gymnasium_id = self._choose_sport()
-        if not gymnasium_id:
+    def appointment(self, file="config.yml"):
+        if datetime.datetime.now().hour < 22:
+            print("Not available time!")
             return
-        sport = {1: '羽毛球', 4: '游泳'}.get(gymnasium_id)
-        print(f"选定运动类型: {sport}")
-        print("正在寻找两天内的可用场地...")
-        available = self.find_available(gymnasium_id)
-        if not available:
-            print("这几天场地已经约满了！")
-            return
-        date = self._choose_date(available)
-        if not date:
-            return
-        print(f"选定日期: {date}")
-        t_id = self._choose_time(available, date)
-        if not t_id:
-            return
-        print(f"选定时间: {self.id2time[t_id]}")
-        available = available[(date, t_id)]
-        place = self._choose_available_place(available)
-        if not place:
-            return
-        print(f"选定场地号: {place}")
-        people_number = self._choose_people_number(gymnasium_id)
-        if not people_number:
-            return
-        is_success, msg = self.submit(gymnasium_id=gymnasium_id,
-                                      sport_place_id=place,
-                                      time_quantum_id=t_id,
-                                      user='',
-                                      people_number=people_number,
-                                      appointment_day=date,
-                                      phone='')
-        if not is_success:
-            print(f"预约失败: {msg}")
+        with open(file, encoding='utf-8') as f:
+            data = yaml.safe_load(f)
+        username = data['username']
+        password = data['password']
+        time_ids = data['time_ids']
+        login_status = self._login(username, password)
+        if login_status:
+            print("Login successfully!")
         else:
-            print(f"预约成功\n此次操作ID为: {msg.get('id')}(可用于取消预约), 您的入场码为: {msg.get('code')}, 场地: {msg.get('placeName')}, "
-                  f"入场时间: {date} {self.id2time[t_id]}, 入场人数: {people_number}")
+            print("Login failed!")
+            return
+        people_number = data.get('people_number', 2)
+        gymnasium_id = 1
+        date = (datetime.datetime.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d"')
+        tasks = []
+        success_list = []
+        for time_id in time_ids:
+            for place in range(1, 15):
+                tasks.append(self.submit(gymnasium_id=gymnasium_id,
+                                         sport_place_id=place,
+                                         time_quantum_id=time_id,
+                                         user='',
+                                         people_number=people_number,
+                                         appointment_day=date,
+                                         phone='',
+                                         success_list=success_list))
+        t = asyncio.gather(*tasks)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(t)
+        if not success_list:
+            print('Available place not found!')
+        for success_item in success_list:
+            print(f"预约成功\n此次操作ID为: {success_item.get('id')}(可用于取消预约), 您的入场码为: {success_item.get('code')},"
+                  f"场地: {success_item.get('placeName')}, 入场时间: {date} {success_item.get('time')}, "
+                  f"入场人数: {people_number}")
